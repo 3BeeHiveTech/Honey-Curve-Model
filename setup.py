47658827d90442fa992c0f1f147c2fb5e92ef1e6
Niccolo Calandri <niccolo.calandri@gmail.com>
2023-07-11 23:12:12 +0200

    Back to Old

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
+"""Python setup.py for honey_curve package"""
 import io
 import os
 from setuptools import find_packages, setup
+from setuptools import setup
 from Cython.Build import cythonize
-from setuptools import Extension
+from setuptools import setup, Extension, Command
 import numpy as np
 
 
 def read(*paths, **kwargs):
-    """Read the contents of a text file safely."""
+    """Read the contents of a text file safely.
+    >>> read("honey_curve", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+
     content = ""
     with io.open(
         os.path.join(os.path.dirname(__file__), *paths),
@@ -25,19 +33,6 @@ def read_requirements(path):
     ]
 
 
-# Specifica i file sorgente per i moduli per ciascuna architettura
-module_sources_x86 = [
-    "honey_curve/sklearn_light/_predictor_x86.pyx",
-    "honey_curve/sklearn_light/_bitset_x86.pyx",
-    "honey_curve/sklearn_light/common_x86.pyx",
-]
-
-module_sources_arm64 = [
-    "honey_curve/sklearn_light/_predictor_arm64.pyx",
-    "honey_curve/sklearn_light/_bitset_arm64.pyx",
-    "honey_curve/sklearn_light/common_arm64.pyx",
-]
-
 setup(
     name="honey_curve",
     version=read("honey_curve", "VERSION.txt"),
@@ -52,41 +47,12 @@ setup(
     extras_require={"test": read_requirements("requirements-test.txt")},
     ext_modules=cythonize(
         [
-            Extension(
-                "honey_curve.sklearn_light._predictor",
-                sources=module_sources_x86,
-                extra_compile_args=['-arch', 'x86_64'],  # Specifica l'architettura x86_64
-                include_dirs=[np.get_include()],
-            ),
-            Extension(
-                "honey_curve.sklearn_light._bitset",
-                sources=module_sources_x86,
-                include_dirs=[np.get_include()],
-            ),
-            Extension(
-                "honey_curve.sklearn_light.common",
-                sources=module_sources_x86,
-                include_dirs=[np.get_include()],
-            ),
-            Extension(
-                "honey_curve.sklearn_light._predictor",
-                sources=module_sources_arm64,
-                extra_compile_args=['-arch', 'arm64'],  # Specifica l'architettura ARM64
-                include_dirs=[np.get_include()],
-            ),
-            Extension(
-                "honey_curve.sklearn_light._bitset",
-                sources=module_sources_arm64,
-                include_dirs=[np.get_include()],
-            ),
-            Extension(
-                "honey_curve.sklearn_light.common",
-                sources=module_sources_arm64,
-                include_dirs=[np.get_include()],
-            ),
+            "honey_curve/sklearn_light/_predictor.pyx",
+            "honey_curve/sklearn_light/_bitset.pyx",
+            "honey_curve/sklearn_light/common.pyx",
         ],
         annotate=True,
     ),
     include_dirs=[np.get_include()],
-    include_package_data=True,
+    include_package_data=True,  # This line is important!
 )
