bb012b9c1ea4a06bce222c89752a1deb576c120e
Axel Dolcemascolo <axel.dolcemascolo@3bee.com>
2023-07-11 14:30:19 +0200

    UPDATE: notebooks

--- notebooks/Load_back_model_light.py
+++ notebooks/Load_back_model_light.py
@@ -8,29 +8,27 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.14.1
 #   kernelspec:
-#     display_name: esabloom_invest_venv
+#     display_name: honey_curve_light
 #     language: python
-#     name: esabloom_invest_venv
+#     name: honey_curve_light
 # ---
 
 # %% [markdown]
 # # Load back the light model
 
 # %% tags=[]
-import cloudpickle
+import pickle
+from importlib.resources import as_file, files
+from pathlib import Path
 
-path_to_model = "m221124_002_barebone.pickle"
+import honey_curve
+
+with as_file(files(honey_curve).joinpath("models", "m221124_002")) as path:
+    path_to_model_folder = Path(path)
+
+# 2. Load the  custom sklearn_light model
+path_to_model = path_to_model_folder / "m221124_002_barebone.pickle"
 with open(path_to_model, "rb") as f:
-    m221124_002_bare = cloudpickle.load(f)
-
-# %% tags=[]
-m221124_002_bare
-
-# %% tags=[]
-path_to_model = "test_sklearn.pickle"
-with open(path_to_model, "rb") as f:
-    sklearn = cloudpickle.load(f)
-
-# %%
+    m221124_002_bare = pickle.load(f)
 
 # %%
